a26c45a3b06c937837b68d0ecfe3ea70031a17cf
Yogesh Jat <yogeshjat.work@gmail.com>
2025-04-22 17:32:11 +0530

    added static files to production

--- KanoonSarathi/settings.py
+++ KanoonSarathi/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
 
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -128,23 +129,18 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, "static"),
-# ]
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR,"app", "static"),
+]
 
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-
-
-# # yogesh jat
-
-# LOGIN_URL = '/accounts/login/'
-# SIGNUP_URL='/accounts/signup/'
 
 
 
