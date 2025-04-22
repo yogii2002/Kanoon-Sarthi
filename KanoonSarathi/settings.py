a4f981dbe48a4495fcec5bdd5078aa4286108cda
Yogesh Jat <yogeshjat.work@gmail.com>
2025-04-22 17:52:44 +0530

    configures static files route for deployment

--- KanoonSarathi/settings.py
+++ KanoonSarathi/settings.py
@@ -16,7 +16,6 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
@@ -130,7 +129,7 @@ STATIC_URL = '/static/'
 
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR,"app", "static"),
+    os.path.join(BASE_DIR, "static"),
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
