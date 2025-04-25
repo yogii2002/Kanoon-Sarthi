4ba54c518f17e6908592b7d2c0c5baa404928d43
Yogesh Jat <yogeshjat.work@gmail.com>
2025-04-25 12:10:48 +0530

    remove crsf middleware

--- KanoonSarathi/settings.py
+++ KanoonSarathi/settings.py
@@ -50,7 +50,6 @@ MIDDLEWARE = [
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
